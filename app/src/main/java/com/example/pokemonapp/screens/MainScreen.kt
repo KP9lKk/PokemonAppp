1aac02283b230247c757ab077c7a80348fb7021c
teacher <teacher@email>
2024-06-15 10:09:48 +0300

    second day

--- app/src/main/java/com/example/pokemonapp/screens/MainScreen.kt
+++ app/src/main/java/com/example/pokemonapp/screens/MainScreen.kt
@@ -1,26 +1,24 @@
 package com.example.pokemonapp.screens
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.view.MenuInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import androidx.fragment.app.commit
-import androidx.fragment.app.replace
+import android.widget.ImageButton
+import androidx.annotation.StyleRes
+import androidx.appcompat.widget.PopupMenu
+import androidx.fragment.app.Fragment
 import com.example.pokemonapp.R
 
 class MainScreen : Fragment(R.layout.fragment_main_screen) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val button = view
-            .findViewById<Button>(R.id.NavigateButton)
-
-        button.setOnClickListener {
-            parentFragmentManager.commit {
-                replace<AboutScreen>(R.id.host_fragment)
-            }
+        val changeModeButton = view.findViewById<ImageButton>(R.id.ChangeMode)
+        changeModeButton.setOnClickListener {
+            val menu = PopupMenu(requireContext(), it)
+            val inflater: MenuInflater = menu.menuInflater
+            inflater.inflate(R.menu.search_menu, menu.menu)
+            menu.show()
         }
     }
 }
